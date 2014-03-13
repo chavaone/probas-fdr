5b7e9d579610b40071342debfcce80bba1f82155
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2014-03-13 13:54:39 +0100

    Implement init function to retrieve parameters.

--- src/ExampleDriver.java
+++ src/ExampleDriver.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
 import com.mundor.fogardixital.core.api.DriverConfiguration;
 import com.mundor.fogardixital.core.api.DriverLifecycle;
 import com.mundor.fogardixital.core.api.PollableComponent;
@@ -5,15 +12,17 @@ import com.mundor.fogardixital.core.api.command.Command;
 import com.mundor.fogardixital.core.api.command.CommandSubscriber;
 import com.mundor.fogardixital.core.api.command.OnCommandPublishListener;
 import com.mundor.fogardixital.core.api.exceptions.ComponentException;
+import com.mundor.fogardixital.core.api.observation.Measure;
+import com.mundor.fogardixital.core.api.observation.Observation;
 import com.mundor.fogardixital.core.api.observation.ObservationPublisher;
 
 
-public class ExampleDriver implements DriverLifecycle, PollableComponent,
-		OnCommandPublishListener {
+public class ExampleDriver implements DriverLifecycle, OnCommandPublishListener {
 	
 	
-	private CommandSubscriber commandSubscriber;
-	private ObservationPublisher observationPublisher;
+	private CommandSubscriber cs;
+	private ObservationPublisher op;
+	private URL wget_url;
 
 	@Override
 	public void destroy() throws ComponentException {
@@ -34,23 +43,53 @@ public class ExampleDriver implements DriverLifecycle, PollableComponent,
 	}
 
 	@Override
-	public void onCommandPublish(Command arg0) {
-		// TODO Auto-generated method stub
+	public void onCommandPublish(Command cmd) {
+		if (cmd == null)
+			return;
+		if (! cmd.getCommand().equals("get"))
+			return;
+		
+		String photo_b64 = this.getPhoto();
+		Measure measure = new Measure.Builder("camera", "photo").value("photo", photo_b64, "string").build();
+		op.publish(new Observation(measure));
 
 	}
 
-	@Override
-	public void poll() {
-		// TODO Auto-generated method stub
+	private String getPhoto(){
 
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(this.wget_url.openStream(), "UTF-8"));) {
+		    
+			String text = "";
+			
+		    for (String line; (line = reader.readLine()) != null;) {
+		        text += line + "\n";
+		    }
+		    
+		    return null;
+		} catch (IOException ignore) {
+			return null;
+		}
+		
+		
 	}
 
 	@Override
-	public void init(DriverConfiguration arg0) throws ComponentException {
+	public void init(DriverConfiguration driverconf) throws ComponentException {
 		// TODO Auto-generated method stub
 		
-		this.commandSubscriber = arg0.getCommandSubscriber();
-		this.observationPublisher = arg0.getObservationPublisher();
+		Map<String, String> parameters = driverconf.getParameters();
+		
+		if (! parameters.containsKey("url"))
+			throw new ComponentException("URL is a mandatory parameter", null);
+		
+		try {
+			this.wget_url = new URL(parameters.get("url"));
+		} catch (MalformedURLException e) {
+			throw new ComponentException("Invalid URL format", e);
+		}
+		
+		this.op = driverconf.getObservationPublisher();
+		driverconf.getCommandSubscriber().subscribe(this, "wget_camera");
 		
 
 	}
