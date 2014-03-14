b4db75fbbbddf26dcdf618e7943b676b7aab6a73
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2014-03-14 12:32:44 +0100

    Implement driver.

--- src/WgetCameraDriver.java
+++ src/WgetCameraDriver.java
@@ -1,26 +1,27 @@
-import java.io.BufferedReader;
+
+
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
 
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.mundor.fogardixital.core.api.DriverConfiguration;
 import com.mundor.fogardixital.core.api.DriverLifecycle;
-import com.mundor.fogardixital.core.api.PollableComponent;
 import com.mundor.fogardixital.core.api.command.Command;
-import com.mundor.fogardixital.core.api.command.CommandSubscriber;
 import com.mundor.fogardixital.core.api.command.OnCommandPublishListener;
 import com.mundor.fogardixital.core.api.exceptions.ComponentException;
 import com.mundor.fogardixital.core.api.observation.Measure;
 import com.mundor.fogardixital.core.api.observation.Observation;
 import com.mundor.fogardixital.core.api.observation.ObservationPublisher;
 
-
-public class ExampleDriver implements DriverLifecycle, OnCommandPublishListener {
+public class WgetCameraDriver implements DriverLifecycle, OnCommandPublishListener {
 	
-	
-	private CommandSubscriber cs;
+    private final static Logger logger = LoggerFactory.getLogger(WgetCameraDriver.class.getPackage().getName());
 	private ObservationPublisher op;
 	private URL wget_url;
 
@@ -49,34 +50,35 @@ public class ExampleDriver implements DriverLifecycle, OnCommandPublishListener 
 		if (! cmd.getCommand().equals("get"))
 			return;
 		
-		String photo_b64 = this.getPhoto();
-		Measure measure = new Measure.Builder("camera", "photo").value("photo", photo_b64, "string").build();
-		op.publish(new Observation(measure));
-
-	}
-
-	private String getPhoto(){
-
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(this.wget_url.openStream(), "UTF-8"));) {
-		    
-			String text = "";
-			
-		    for (String line; (line = reader.readLine()) != null;) {
-		        text += line + "\n";
-		    }
-		    
-		    return null;
-		} catch (IOException ignore) {
-			return null;
+		logger.info("GET received");
+		
+		String photo_b64;
+		try {
+			photo_b64 = this.getPhoto();
+			Measure measure = new Measure.Builder("camera", "photo").value("photo", photo_b64, "string").build();
+			op.publish(new Observation(measure));
+			logger.info("Send Photo");
+		} catch (IOException e) {
+			logger.error("Error getting photo:" + e.getLocalizedMessage());
 		}
 		
-		
+
+	}
+	
+	private String getPhoto() throws IOException{
+
+		try (InputStream openStream = this.wget_url.openStream();)  {
+			int contentLength = this.wget_url.openConnection().getContentLength();
+			byte[] img_bytes = new byte[contentLength];
+			openStream.read(img_bytes);
+			return Base64.encodeBase64(img_bytes).toString();
+		} catch(IOException e) {
+			throw e;
+		}
 	}
 
 	@Override
-	public void init(DriverConfiguration driverconf) throws ComponentException {
-		// TODO Auto-generated method stub
-		
+	public void init(DriverConfiguration driverconf) throws ComponentException {			
 		Map<String, String> parameters = driverconf.getParameters();
 		
 		if (! parameters.containsKey("url"))
@@ -93,5 +95,4 @@ public class ExampleDriver implements DriverLifecycle, OnCommandPublishListener 
 		
 
 	}
-
 }
